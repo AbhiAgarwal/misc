83646bfce15533fe0a4e358a55604b9fbe4f45d8
Abhi Agarwal <me@abhiagarwal.com>
2015-01-26 19:04:17 -0500

    Fixing

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# goalgorithms
+# Go Algorithms
+
+Collections of algorithms to easily use in Go. I haven't implemented all of them myself, and have referenced the ones I've borrowed.
+
+```
+go get github.com/AbhiAgarwal/alGO
+```
+
+**Algorithm's Implemented**
+
+- Sort
+	- Mergesort
+	- Quicksort
--- sort/mergesort_test.go
+++ sort/mergesort_test.go
@@ -6,12 +6,24 @@ import (
 	"testing"
 )
 
-func TestQuickSort(t *testing.T) {
+func IntArrayEquals(a []int, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestMergeSort(t *testing.T) {
 	randomArray := make([]int, 20)
 	for i := range randomArray {
 		randomArray[i] = rand.Intn(10)
 	}
-	randomArray = QuickSort(randomArray)
+	randomArray = MergeSort(randomArray)
 	correctArray := sort.IntSlice(randomArray)
 	if !IntArrayEquals(randomArray, correctArray) {
 		t.Error("Array is not correct")
