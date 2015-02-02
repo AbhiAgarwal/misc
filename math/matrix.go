9ed8fad85022ec7f4069666feec82604b184f43c
Abhi Agarwal <me@abhiagarwal.com>
2015-02-02 17:48:46 -0500

    Adding row reduction

--- math/matrix.go
+++ math/matrix.go
@@ -49,8 +49,45 @@ func (m *Matrix) GetSize() (rows, cols int) {
 func (m *Matrix) PrintMatrix() {
 	for i := 0; i < m.Rows; i++ {
 		for x := 0; x < m.Cols; x++ {
-			fmt.Printf("%.0f ", m.M[i][x])
+			fmt.Printf("%.0f\t", m.M[i][x])
 		}
 		fmt.Println("")
+	}
+}
+
+// reference: http://rosettacode.org/wiki/Reduced_row_echelon_form#2D_representation
+func (m *Matrix) RowReduction() {
+	lead := 0
+	rowCount := m.Rows
+	columnCount := m.Cols
+	for r := 0; r < rowCount; r++ {
+		if lead >= columnCount {
+			return
+		}
+		i := r
+		for m.M[i][lead] == 0 {
+			i++
+			if rowCount == i {
+				i = r
+				lead++
+				if columnCount == lead {
+					return
+				}
+			}
+		}
+		m.M[i], m.M[r] = m.M[r], m.M[i]
+		f := 1 / m.M[r][lead]
+		for j, _ := range m.M[r] {
+			m.M[r][j] *= f
+		}
+		for i = 0; i < rowCount; i++ {
+			if i != r {
+				f = m.M[i][lead]
+				for j, e := range m.M[r] {
+					m.M[i][j] -= e * f
+				}
+			}
+		}
+		lead++
 	}
 }
--- math/matrix_test.go
+++ math/matrix_test.go
@@ -35,3 +35,37 @@ func TestGetSize(t *testing.T) {
 		t.Error("Matrix sizes are incorrect")
 	}
 }
+
+func compare(X, Y [][]float64) bool {
+	for i := 0; i < len(X); i++ {
+		for x := 0; x < len(X[0]); x++ {
+			if X[i][x] != Y[i][x] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func TestRowReduction(t *testing.T) {
+	m := Matrix{}
+	m.Matrix(3, 4)
+	m.M = [][]float64{
+		{1, 2, -1, -4},
+		{2, 3, -1, -11},
+		{-2, 0, -3, 22},
+	}
+	m.RowReduction()
+
+	correctMatrix := Matrix{}
+	correctMatrix.Matrix(3, 4)
+	correctMatrix.M = [][]float64{
+		{1, 0, 0, -8},
+		{-0, 1, 0, 1},
+		{-0, -0, 1, -2},
+	}
+
+	if !compare(correctMatrix.M, m.M) {
+		t.Error("Row reduction not working!")
+	}
+}
